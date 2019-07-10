97ae05c2291147d7f79a34d9cb7e76fadd259cb6
Joumana El Alaoui <joumana.elalaoui@gmail.com>
2019-07-10 16:32:34 -0500

    pomodoro timer

--- day1_datetime.py
+++ day1_datetime.py
@@ -1,10 +1,21 @@
-#Decide on the task to be done.
-#Set the pomodoro timer (traditionally to 25 minutes).[1]
-#Work on the task.
-#End work when the timer rings and put a checkmark on a piece of paper.[5]
-#If you have fewer than four checkmarks, take a short break (3–5 minutes), then go to step 2.
-#After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
+#pomodoro timer
+
+import time
 
 
-from datetime import datetime
-from datetime import timedelta
+workTimeSec = 1500
+breakTimeSec = 300
+
+workTimeStartMessage = "Worktime has started. You have 25 minutes!"
+workTimeDone = "Time to break!"
+
+startTime = time.time()
+
+print("Worktime has started. You have 25 minutes!")
+while time.time() != startTime + workTimeSec:
+    pass
+
+breakTimerStartAlert = print(workTimeDone)
+startTime = time.time()
+while time.time() != startTime + breakTimeSec:
+    pass
